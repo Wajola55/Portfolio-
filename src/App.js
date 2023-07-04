65b74fa7e218e6561660e39f105240ece3499668
Wajola55 <koczorwioletta@gmail.com>
2023-07-04 10:14:45 +0100

    code modification

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ import Experience from './components/Experience/Experience';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
-import Toggle from './components/Toggle/Toggle';
 import {themeContext} from './Context'
 import { useContext } from 'react';
-
-
 
 function App() {
   const theme = useContext(themeContext);
@@ -28,7 +25,6 @@ function App() {
     <Projects/>
     <Contact/>
     <Footer/>
-    <Toggle/>
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,56 @@
 import React from 'react';
-import Toggle from '../Toggle/Toggle'
-import '/home/wiola/Desktop/Portfolio/src/components/Navbar/Navbar.css'
-import {Link} from 'react-scroll'
+import Toggle from '../Toggle/Toggle';
+import '/home/wiola/Desktop/Portfolio/src/components/Navbar/Navbar.css';
+import { Link } from 'react-scroll';
 
+import ukFlag from '../../img//UK.png';
+import polandFlag from '../../img//Poland.png';
 
 const Navbar = () => {
+  const redirectToPolishSite = () => {
+    window.open('https://wkoczor.pl/PL', '_blank');
+  };
+
   return (
     <div className="n-wrapper">
       <div className="n-left">
         <div className="n-name">Wioletta</div>
-        <Toggle/>
+        <Toggle />
       </div>
       <div className="n-right">
         <div className="n-list">
-          <ul style={{listStyleType: 'none'}}>
-
-            <Link spy={true} to='Navbar' smooth={true} activateClass='activeClass'>
-            <li>Home</li>
+          <ul style={{ listStyleType: 'none' }}>
+            <Link spy={true} to="Navbar" smooth={true} activateClass="activeClass">
+              <li>Home</li>
             </Link>
-            <Link spy={true} to='Skills' smooth={true}>
-            <li>Skills</li>
+            <Link spy={true} to="Skills" smooth={true}>
+              <li>Skills</li>
             </Link>
-            <Link spy={true} to='Experience' smooth={true}>
-            <li>Experience</li>
+            <Link spy={true} to="Experience" smooth={true}>
+              <li>Experience</li>
             </Link>
-            <Link spy={true} to='Projects' smooth={true}>
-            <li>Projects</li>
+            <Link spy={true} to="Projects" smooth={true}>
+              <li>Projects</li>
             </Link>
+            <li>
+              <button className="flag-button" onClick={() => console.log('Switch to EN')}>
+                <img src={ukFlag} alt="English Flag" className="flag-img" />
+              </button>
+            </li>
+            <li>
+            <button className="flag-button" onClick={redirectToPolishSite}>
+                <img src={polandFlag} alt="Polish Flag" className="flag-img" />
+              </button>
+            </li>
           </ul>
         </div>
         <Link spy={true} to="Contact" smooth={true}>
-        <button className="button n-button">
-          Contact
-        </button>
+          <button className="button n-button">Contact</button>
         </Link>
       </div>
     </div>
   );
 };
 
-export default Navbar
+export default Navbar;
+
